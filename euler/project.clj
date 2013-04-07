627cf26218c54c34a0d935e19c6fb7afdeaf850d
Chris Johnson <hlprmnky@gmail.com>
2013-04-07 11:27:04 -0400

    Add midje dependency to :dev profile for autotest

--- euler/project.clj
+++ euler/project.clj
@@ -1,6 +1,7 @@
 (defproject euler "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Working through Project Euler problems with Clojure and Leiningen"
+  :url "http://github.com/hlprmnky/clojure-euler"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.5.0"]]
+  :profiles {:dev {:dependencies [[midje "1.5.0"]]}})
